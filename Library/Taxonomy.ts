735a58377ef51ae63aedcb8b13cde9d5832d2613
RemkoJ <693172+remkoj@users.noreply.github.com>
2020-09-11 16:05:14 +0200

    Extended Taxonomy

--- Library/Taxonomy.ts
+++ Library/Taxonomy.ts
@@ -1,5 +1,9 @@
 import * as IContentNS from '../Models/IContent';
 import * as ContentLinkNS from '../Models/ContentLink';
+import * as WebsiteNS from '../Models/Website';
+import * as WebsiteListNS from '../Models/WebsiteList';
+import * as LanguageNS from '../Models/Language';
+import * as LanguageListNS from '../Models/LanguageList';
 
 // Types
 export type IContent = IContentNS.default
@@ -7,6 +11,10 @@ export type IContentType = IContentNS.IContentType;
 export type ContentLink = ContentLinkNS.default;
 export type ContentReference = ContentLinkNS.ContentReference;
 export type ContentApiId = ContentLinkNS.ContentApiId;
+export type Website = WebsiteNS.default;
+export type WebsiteList = WebsiteListNS.default;
+export type Language = LanguageNS.default;
+export type LanguageList = LanguageListNS.default;
 
 // Classes
 export const AbstractIContent = IContentNS.BaseIContent;
