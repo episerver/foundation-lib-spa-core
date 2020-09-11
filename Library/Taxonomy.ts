ca7da91770fda5368c5fcd2fe383d84f15b9ebbd
RemkoJ <693172+remkoj@users.noreply.github.com>
2020-09-11 14:10:33 +0200

    Exposed content link service (2)

--- Library/Taxonomy.ts
+++ Library/Taxonomy.ts
@@ -1,9 +1,13 @@
 import * as IContentNS from '../Models/IContent';
 import * as ContentLinkNS from '../Models/ContentLink';
 
+// Types
 export type IContent = IContentNS.default
 export type IContentType = IContentNS.IContentType;
-export const AbstractIContent = IContentNS.BaseIContent;
 export type ContentLink = ContentLinkNS.default;
 export type ContentReference = ContentLinkNS.ContentReference;
 export type ContentApiId = ContentLinkNS.ContentApiId;
+
+// Classes
+export const AbstractIContent = IContentNS.BaseIContent;
+export const ContentLinkService = ContentLinkNS.ContentLinkService;
