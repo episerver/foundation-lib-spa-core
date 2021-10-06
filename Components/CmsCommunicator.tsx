9529efdacd9cb9642d1236745e3bd22a7d0980a7
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2021-10-06 13:24:16 +0000

    Changed loading condition to prevent issues due to early loading of the communicator

--- Components/CmsCommunicator.tsx
+++ Components/CmsCommunicator.tsx
@@ -26,7 +26,7 @@ export const CmsCommunicator : FunctionComponent<CmsCommunicatorProps> = (props)
     const myScriptPath = props.scriptPath || ScriptPath;
     const myScriptUrl = context.getEpiserverUrl(myScriptPath + myScriptFile);
 
-    const shouldLoadScript = (context.isEditable() || context.isInEditMode()) && (!context.isServerSideRendering() && !communicatorLoaded(myScriptFile))
+    const shouldLoadScript = (context.isEditable() || context.isInEditMode()) && !context.isServerSideRendering()
     const scriptHrf = myScriptUrl.href;
 
     if (context.isEditable() || context.isInEditMode())
