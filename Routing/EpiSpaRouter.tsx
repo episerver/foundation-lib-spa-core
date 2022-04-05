e12cdf04f2498e111965148e7abb087f96537f64
matand-dd <mathias.andersson@dynamicdog.se>
2022-04-05 11:39:42 +0200

    #master debug ssr.

--- Routing/EpiSpaRouter.tsx
+++ Routing/EpiSpaRouter.tsx
@@ -1,139 +1,171 @@
 import React, { useEffect } from 'react';
-import { StaticRouter, StaticRouterProps, useHistory, useLocation, Switch, SwitchProps, Route, RouteProps, RouteComponentProps } from 'react-router';
+import {
+  StaticRouter,
+  StaticRouterProps,
+  useHistory,
+  useLocation,
+  Switch,
+  SwitchProps,
+  Route,
+  RouteProps,
+  RouteComponentProps,
+} from 'react-router';
 import { BrowserRouter, BrowserRouterProps } from 'react-router-dom';
 import IRouteConfig, { IRouteConfigItem } from './IRouteConfig';
 import IEpiserverContext from '../Core/IEpiserverContext';
 import { useEpiserver } from '../Hooks/Context';
 
 export type RouterProps = StaticRouterProps & BrowserRouterProps;
-export const Router : React.FunctionComponent<RouterProps> = (props) =>
-{
-    const epi = useEpiserver();
+export const Router: React.FunctionComponent<RouterProps> = (props) => {
+  const epi = useEpiserver();
 
-    if (epi.isServerSideRendering()) {
-        const staticRouterProps : StaticRouterProps = {
-            basename: props.basename,
-            context: props.context,
-            location: props.location
-        };
-        return <StaticRouter {...staticRouterProps}>{ props.children }</StaticRouter>
-    }
-
-    const browserRouterProps : BrowserRouterProps = {
-        basename: props.basename,
-        forceRefresh: props.forceRefresh,
-        getUserConfirmation: props.getUserConfirmation,
-        keyLength: props.keyLength
+  if (epi.isServerSideRendering()) {
+    const staticRouterProps: StaticRouterProps = {
+      basename: props.basename,
+      context: props.context,
+      location: props.location,
     };
+    return <StaticRouter {...staticRouterProps}>{props.children}</StaticRouter>;
+  }
 
-    if (epi.isInEditMode() || epi.isEditable())
-        return <BrowserRouter {...browserRouterProps}>{ props.children }</BrowserRouter>
+  const browserRouterProps: BrowserRouterProps = {
+    basename: props.basename,
+    forceRefresh: props.forceRefresh,
+    getUserConfirmation: props.getUserConfirmation,
+    keyLength: props.keyLength,
+  };
 
-    return <BrowserRouter {...browserRouterProps}><ElementNavigation>{ props.children }</ElementNavigation></BrowserRouter>
-}
-Router.displayName = "Optimizely CMS: Router";
+  if (epi.isInEditMode() || epi.isEditable())
+    return <BrowserRouter {...browserRouterProps}>{props.children}</BrowserRouter>;
+
+  return (
+    <BrowserRouter {...browserRouterProps}>
+      <ElementNavigation>{props.children}</ElementNavigation>
+    </BrowserRouter>
+  );
+};
+Router.displayName = 'Optimizely CMS: Router';
 export default Router;
 
-const ElementNavigation : React.FunctionComponent = (props) : React.ReactElement => {
-    const history = useHistory();
-    const location = useLocation();
-    const epi = useEpiserver();
-    const config = epi.config();
+const ElementNavigation: React.FunctionComponent = (props): React.ReactElement => {
+  const history = useHistory();
+  const location = useLocation();
+  const epi = useEpiserver();
+  const config = epi.config();
 
-    useEffect(() => {
-        if (epi.isInEditMode() || epi.isServerSideRendering()) {
-            if (epi.isDebugActive()) console.info('ElementNavigation: Edit mode, or SSR, so not attaching events');
-            return;
-        } else {
-            if (epi.isDebugActive()) console.info('ElementNavigation: Enabling catch-all click handling for navigation');
+  useEffect(() => {
+    if (epi.isInEditMode() || epi.isServerSideRendering()) {
+      if (epi.isDebugActive()) console.info('ElementNavigation: Edit mode, or SSR, so not attaching events');
+      return;
+    } else {
+      if (epi.isDebugActive()) console.info('ElementNavigation: Enabling catch-all click handling for navigation');
+    }
+    const onWindowClick = (event: MouseEvent) => {
+      const target: HTMLElement = event.target as any as HTMLElement;
+      const currentUrl: URL = new URL(window.location.href);
+      let newPath = '';
+
+      // Loop parents till we find the link
+      let link = target;
+      while (link.parentElement && link.tagName.toLowerCase() !== 'a') link = link.parentElement;
+
+      // If we have a link, see if we need to navigate
+      if (link.tagName.toLowerCase() === 'a') {
+        const targetUrl: URL = new URL((link as HTMLAnchorElement).href, currentUrl);
+
+        // Only act if we remain on the same domain
+        if (targetUrl.origin === currentUrl.origin) {
+          newPath = targetUrl.pathname;
         }
-        const onWindowClick = (event: MouseEvent) => {
-            const target: HTMLElement = (event.target as any) as HTMLElement;
-            const currentUrl: URL = new URL(window.location.href);
-            let newPath = '';
+      }
 
-            // Loop parents till we find the link
-            let link = target;
-            while (link.parentElement && link.tagName.toLowerCase() !== 'a') link = link.parentElement;
+      // Do not navigate to the same page
+      if (newPath === location.pathname) {
+        if (config.enableDebug) console.info('ElementNavigation: Ignoring navigation to same path');
+        event.preventDefault();
+        return false;
+      }
 
-            // If we have a link, see if we need to navigate
-            if (link.tagName.toLowerCase() === 'a') {
-                const targetUrl: URL = new URL((link as HTMLAnchorElement).href, currentUrl);
-
-                // Only act if we remain on the same domain
-                if (targetUrl.origin === currentUrl.origin) {
-                    newPath = targetUrl.pathname;
-                }
-            }
-
-            // Do not navigate to the same page
-            if (newPath === location.pathname) {
-                if (config.enableDebug) console.info('ElementNavigation: Ignoring navigation to same path');
-                event.preventDefault();
-                return false;
-            }
-            
-            // Navigate to the new path
-            if (newPath) {
-                if (config.basePath && newPath.substr(0, config.basePath.length) === config.basePath) {
-                    newPath = newPath.substr(config.basePath.length);
-                    if (newPath.substr(0, 1) !== '/') newPath = '/' + newPath; // Ensure we've an absolute path
-                }
-                history.push(newPath);
-                event.preventDefault();
-
-                return false;
-            }
+      // Navigate to the new path
+      if (newPath) {
+        if (config.basePath && newPath.substr(0, config.basePath.length) === config.basePath) {
+          newPath = newPath.substr(config.basePath.length);
+          if (newPath.substr(0, 1) !== '/') newPath = '/' + newPath; // Ensure we've an absolute path
         }
+        history.push(newPath);
+        event.preventDefault();
 
-        try {
-            window.scrollTo(0,0);
-        } catch (e) {
-            if (epi.isDebugActive()) console.warn('ElementNavigation: Failed to scroll to top');
-        }
-        document.addEventListener('click', onWindowClick);
-        return () => {
-            if (epi.isDebugActive()) console.info('ElementNavigation: Removing catch-all click handling for navigation');
-            document.removeEventListener('click', onWindowClick);
-        }
-    });
+        return false;
+      }
+    };
 
-    return props.children as React.ReactElement;
-}
-ElementNavigation.displayName = "Optimizely CMS: Generic click event handler";
+    try {
+      window.scrollTo(0, 0);
+    } catch (e) {
+      if (epi.isDebugActive()) console.warn('ElementNavigation: Failed to scroll to top');
+    }
+    document.addEventListener('click', onWindowClick);
+    return () => {
+      if (epi.isDebugActive()) console.info('ElementNavigation: Removing catch-all click handling for navigation');
+      document.removeEventListener('click', onWindowClick);
+    };
+  });
+
+  return props.children as React.ReactElement;
+};
+ElementNavigation.displayName = 'Optimizely CMS: Generic click event handler';
 
 export type RoutedContentProps = SwitchProps & {
-    keyPrefix ?:    string,
-    config ?:       IRouteConfig,
-    basePath ?:     string
-}
+  keyPrefix?: string;
+  config?: IRouteConfig;
+  basePath?: string;
+};
 
-export const RoutedContent : React.FunctionComponent<RoutedContentProps> = (props) => {
-    const ctx = useEpiserver();
-    const switchProps : SwitchProps = { location: props.location }
-    return <Switch {...switchProps}>
-        { props.children }
-        { (props.config || []).map( (item, idx) => createRouteNode(item, props.basePath, `${props.keyPrefix}-route-${idx}`, ctx) ) }
+export const RoutedContent: React.FunctionComponent<RoutedContentProps> = (props) => {
+  const ctx = useEpiserver();
+  const switchProps: SwitchProps = { location: props.location };
+  return (
+    <Switch {...switchProps}>
+      {props.children}
+      {(props.config || []).map((item, idx) =>
+        createRouteNode(item, props.basePath, `${props.keyPrefix}-route-${idx}`, ctx),
+      )}
     </Switch>
-}
-RoutedContent.displayName = "Optimizely CMS: Route container";
+  );
+};
+RoutedContent.displayName = 'Optimizely CMS: Route container';
 
-function createRouteNode(route: IRouteConfigItem, basePath = "", key ?: string, ctx ?: IEpiserverContext) : React.ReactElement<RouteProps> {
-    
-    let createdRoute : string = basePath ? (basePath.substr(-1) === "/" ? basePath.substr(0, -1) : basePath) : "";
-    createdRoute = createdRoute + "/" + (route.path ? (route.path.substr(0,1) === "/" ? route.path.substr(1) : route.path) : "")
+function createRouteNode(
+  route: IRouteConfigItem,
+  basePath = '',
+  key?: string,
+  ctx?: IEpiserverContext,
+): React.ReactElement<RouteProps> {
+  let createdRoute: string = basePath ? (basePath.substr(-1) === '/' ? basePath.substr(0, -1) : basePath) : '';
+  createdRoute =
+    createdRoute + '/' + (route.path ? (route.path.substr(0, 1) === '/' ? route.path.substr(1) : route.path) : '');
 
-    if (ctx?.isDebugActive()) console.log('Generating Route Virtual DOM Node', createdRoute, route, key);
+  //   if (ctx?.isDebugActive())
+  console.warn('Generating Route Virtual DOM Node', createdRoute, route, key);
 
-    const newRouteProps : RouteProps = {
-        children: route.children,
-        exact: route.exact,
-        location: route.location,
-        path: createdRoute,
-        sensitive: route.sensitive,
-        strict: route.strict,
-        render: route.render ? (props: RouteComponentProps) => { return route.render ? route.render({ ...props, routes: route.routes, path: route.path }) : <div/> } : undefined,
-        component: route.component ? (props: RouteComponentProps) => { const RouteComponent = route.component || 'div'; return <RouteComponent { ...props } routes={ route.routes } path={ route.path } />} : undefined
-    }
-    return <Route { ...newRouteProps } key={ key } />
+  const newRouteProps: RouteProps = {
+    children: route.children,
+    exact: route.exact,
+    location: route.location,
+    path: createdRoute,
+    sensitive: route.sensitive,
+    strict: route.strict,
+    render: route.render
+      ? (props: RouteComponentProps) => {
+          return route.render ? route.render({ ...props, routes: route.routes, path: route.path }) : <div />;
+        }
+      : undefined,
+    component: route.component
+      ? (props: RouteComponentProps) => {
+          const RouteComponent = route.component || 'div';
+          return <RouteComponent {...props} routes={route.routes} path={route.path} />;
+        }
+      : undefined,
+  };
+  return <Route {...newRouteProps} key={key} />;
 }
--- dist/Routing/EpiSpaRouter.js
+++ dist/Routing/EpiSpaRouter.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { StaticRouter, useHistory, useLocation, Switch, Route } from 'react-router';
+import { StaticRouter, useHistory, useLocation, Switch, Route, } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { useEpiserver } from '../Hooks/Context';
 export const Router = (props) => {
@@ -8,7 +8,7 @@ export const Router = (props) => {
         const staticRouterProps = {
             basename: props.basename,
             context: props.context,
-            location: props.location
+            location: props.location,
         };
         return React.createElement(StaticRouter, Object.assign({}, staticRouterProps), props.children);
     }
@@ -16,14 +16,14 @@ export const Router = (props) => {
         basename: props.basename,
         forceRefresh: props.forceRefresh,
         getUserConfirmation: props.getUserConfirmation,
-        keyLength: props.keyLength
+        keyLength: props.keyLength,
     };
     if (epi.isInEditMode() || epi.isEditable())
         return React.createElement(BrowserRouter, Object.assign({}, browserRouterProps), props.children);
-    return React.createElement(BrowserRouter, Object.assign({}, browserRouterProps),
-        React.createElement(ElementNavigation, null, props.children));
+    return (React.createElement(BrowserRouter, Object.assign({}, browserRouterProps),
+        React.createElement(ElementNavigation, null, props.children)));
 };
-Router.displayName = "Optimizely CMS: Router";
+Router.displayName = 'Optimizely CMS: Router';
 export default Router;
 const ElementNavigation = (props) => {
     const history = useHistory();
@@ -91,20 +91,21 @@ const ElementNavigation = (props) => {
     });
     return props.children;
 };
-ElementNavigation.displayName = "Optimizely CMS: Generic click event handler";
+ElementNavigation.displayName = 'Optimizely CMS: Generic click event handler';
 export const RoutedContent = (props) => {
     const ctx = useEpiserver();
     const switchProps = { location: props.location };
-    return React.createElement(Switch, Object.assign({}, switchProps),
+    return (React.createElement(Switch, Object.assign({}, switchProps),
         props.children,
-        (props.config || []).map((item, idx) => createRouteNode(item, props.basePath, `${props.keyPrefix}-route-${idx}`, ctx)));
+        (props.config || []).map((item, idx) => createRouteNode(item, props.basePath, `${props.keyPrefix}-route-${idx}`, ctx))));
 };
-RoutedContent.displayName = "Optimizely CMS: Route container";
-function createRouteNode(route, basePath = "", key, ctx) {
-    let createdRoute = basePath ? (basePath.substr(-1) === "/" ? basePath.substr(0, -1) : basePath) : "";
-    createdRoute = createdRoute + "/" + (route.path ? (route.path.substr(0, 1) === "/" ? route.path.substr(1) : route.path) : "");
-    if (ctx === null || ctx === void 0 ? void 0 : ctx.isDebugActive())
-        console.log('Generating Route Virtual DOM Node', createdRoute, route, key);
+RoutedContent.displayName = 'Optimizely CMS: Route container';
+function createRouteNode(route, basePath = '', key, ctx) {
+    let createdRoute = basePath ? (basePath.substr(-1) === '/' ? basePath.substr(0, -1) : basePath) : '';
+    createdRoute =
+        createdRoute + '/' + (route.path ? (route.path.substr(0, 1) === '/' ? route.path.substr(1) : route.path) : '');
+    //   if (ctx?.isDebugActive())
+    console.warn('Generating Route Virtual DOM Node', createdRoute, route, key);
     const newRouteProps = {
         children: route.children,
         exact: route.exact,
@@ -112,8 +113,17 @@ function createRouteNode(route, basePath = "", key, ctx) {
         path: createdRoute,
         sensitive: route.sensitive,
         strict: route.strict,
-        render: route.render ? (props) => { return route.render ? route.render(Object.assign(Object.assign({}, props), { routes: route.routes, path: route.path })) : React.createElement("div", null); } : undefined,
-        component: route.component ? (props) => { const RouteComponent = route.component || 'div'; return React.createElement(RouteComponent, Object.assign({}, props, { routes: route.routes, path: route.path })); } : undefined
+        render: route.render
+            ? (props) => {
+                return route.render ? route.render(Object.assign(Object.assign({}, props), { routes: route.routes, path: route.path })) : React.createElement("div", null);
+            }
+            : undefined,
+        component: route.component
+            ? (props) => {
+                const RouteComponent = route.component || 'div';
+                return React.createElement(RouteComponent, Object.assign({}, props, { routes: route.routes, path: route.path }));
+            }
+            : undefined,
     };
     return React.createElement(Route, Object.assign({}, newRouteProps, { key: key }));
 }
